2538fea6337a9e4672082ad62e75eec0e70a26b4
Jack Sweeney <jackcsweeney@gmail.com>
2024-03-01 12:16:19 -0800

    feat: Add ability to create new bulk discounts from the merchant bulk discount index page

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -1,10 +1,32 @@
 class BulkDiscountsController < ApplicationController
 
   def index
-    @bulk_discounts = Merchant.find(params[:merchant_id]).bulk_discounts
+    @merchant = Merchant.find(params[:merchant_id])
+    @bulk_discounts = @merchant.bulk_discounts
   end
 
   def show
+  end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def create
+    merchant = Merchant.find(params[:merchant_id])
+    bulk_discount = BulkDiscount.new(bulk_discount_params)
+    if bulk_discount.save
+      redirect_to merchant_bulk_discounts_path(merchant)
+    else
+      redirect_to new_merchant_bulk_discount_path(merchant)
+      flash[:error] = "Error: #{error_message(bulk_discount.errors)}"
+    end
+  end
+
+  private
+
+  def bulk_discount_params
+    params.permit(:quantity_thresh, :percentage, :merchant_id)
   end
 
 end
--- spec/features/merchants/bulk_discounts/new_spec.rb
+++ spec/features/merchants/bulk_discounts/new_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe 'Merchant Bulk Discounts New Page', type: :feature do
     # User story solo #2: Merchant Bulk Discount Create
     it 'has a link to create a new bulk discount from the bulk discount index and when used, will add a new bulk discount to the merchants bulk discount index page if all data is input correctly' do
       # As a merchant, when I visit my bulk discounts index
-      merchant_bulk_discounts_path(@merch_1)
+      visit merchant_bulk_discounts_path(@merch_1)
       # Then I see a link to create a new discount
       expect(page).to have_link("Create New Bulk Discount", href: new_merchant_bulk_discount_path(@merch_1))
       # When I click this link
@@ -18,14 +18,14 @@ RSpec.describe 'Merchant Bulk Discounts New Page', type: :feature do
       expect(page).to have_field("percentage")
       # When I fill in the form with valid data
       fill_in("quantity_thresh", with: 15)
-      fill_in("percentage", with: 35)
-      click_on "submit"
+      fill_in("percentage", with: 0.35)
+      click_on "Save"
 
       # Then I am redirected back to the bulk discount index
       # And I see my new bulk discount listed     
       expect(current_path).to eq(merchant_bulk_discounts_path(@merch_1))
       within "#bulk_discounts" do
-        expect(page).to have_content("Quantity - 15, Discount - 35.0%")
+        expect(page).to have_content("Discount ##{@merch_1.bulk_discounts[0].id}: Quantity - 15, Discount - 35.0%")
       end
     end
 
@@ -33,16 +33,11 @@ RSpec.describe 'Merchant Bulk Discounts New Page', type: :feature do
       visit new_merchant_bulk_discount_path(@merch_1)
 
       fill_in("quantity_thresh", with: 15)
-      click_on "submit"
-
-      expect(page).to have_content("Error: Quantity Threshold can't be blank")
-      expect(page).to have_field("quantity_thresh")
-      expect(page).to have_field("percentage")
-
-      fill_in("percentage", with: 35)
-      click_on "submit"
-
-      expect(page).to have_content("Error: Percentage can't be blank")
+      click_on "Save"
+      
+      within "#flash" do
+        expect(page).to have_content("Error: Percentage can't be blank")
+      end
       expect(page).to have_field("quantity_thresh")
       expect(page).to have_field("percentage")
     end
